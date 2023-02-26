b114855fa76081e89372734e0ace8888771796c2
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2023-02-26 04:43:24 -0600

    Fix 'error' to 'err' on sendErrorProd()

--- error/errorController.js
+++ error/errorController.js
@@ -31,6 +31,6 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(error, res);
+    sendErrorProd(err, res);
   }
 };
